6b73d3ecfec97b8c6d0547f09ae6c7998a3182e6
Juan-Clay <123994025+Juan-Clay@users.noreply.github.com>
2023-11-26 19:17:57 -0500

    Add files via upload
    Updated main with classes and functions (Not defined yet)

---
+++ main.py
@@ -0,0 +1,38 @@
+
+
+
+#Will create a list object, might go ahead and make it a linked list to add a bit of difficulty and to have several properties per item 
+#Doubly linked list cause it might come in handy when sorting
+
+
+#"Node" class for this program
+class Item:
+    def __init__(self) -> None:
+        pass
+
+#Will create the first nodde upon creation and house many different functions in order to give functionality to the list
+class List:
+
+    #Initialize list by creating header node
+    def __init__(self) -> None:
+        pass
+
+    #Add specified item
+    def addItem(self, item):
+        pass
+
+    #Remove specified item
+    def removeItem(self, item):
+        pass
+
+    #Orders the whole list by priority, some items will have equal priorites
+    def orderByPriority(self):
+        pass
+
+    #Updates the specified item, might make helper functions in order to do this once since I plan on making it prompt the user what they would like to change (Priority, Name, Description, Due date, ect)
+    def updateItem(self):
+        pass
+    
+
+
+
